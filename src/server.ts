5454e10c579c7468e7ec2c88e5b59755b3e0d19f
tawan475 <43491902+tawan475@users.noreply.github.com>
2025-08-25 22:10:44 +0700

    feat(cors): add CORS middleware and update type definitions

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import dotenv from "dotenv/config";
 import rootRouter from "./Routes/index-route.js";
+import cors from "cors"; // Import the cors middleware
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = process.env.PORT || 300;
 
-app.get('/', (req, res) => {
-  return res.send("Hello World from cultura.com");
-})
+app.get("/", (req, res) => {
+    return res.send("Hello World from cultura.com");
+});
 
 app.use("/api", rootRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
