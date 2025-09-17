66bdde709e92413c71c3577e7d7b90c1518c8b13
Natthanan04 <natthanansirisurayut@gmail.com>
2025-09-17 09:52:23 +0700

    feat(server): add cookie parser and configure CORS for frontend integration

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,27 @@
 import express from "express";
-import dotenv from "dotenv/config";
 import rootRouter from "./Routes/index-route.js";
-import cors from "cors"; // Import the cors middleware
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:4000", // origin ของ front-end
+    credentials: true, // อนุญาตให้ส่ง cookie/header credentials
+  })
+);
 app.use(express.json());
-app.use(cors());
-const port = process.env.PORT || 300;
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    return res.send("Hello World from cultura.com");
+  return res.send("Hello World from cultura.com");
 });
 
 app.use("/api", rootRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
