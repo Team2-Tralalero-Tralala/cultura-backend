25494ab3057935c613201e2807c96e60cc6d9426
Natthanan04 <natthanansirisurayut@gmail.com>
2025-09-24 19:45:52 +0700

    fix(index-route): add community route and delete role route

--- src/Routes/index-route.ts
+++ src/Routes/index-route.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth-route.js";
-import roleRoutes from "./role-route.js";
+import communityRoutes from "./community-route.js";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRoutes);
-rootRouter.use("/role", roleRoutes);
+rootRouter.use("/communities", communityRoutes);
 
 export default rootRouter;
