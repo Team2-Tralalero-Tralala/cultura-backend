56988c602f2f0168bda03247ef8a2fcf85cab91d
Natthanan04 <natthanansirisurayut@gmail.com>
2025-09-28 15:47:00 +0700

    refactor(routes): clean up role-related modules and update index-route.ts
    - deleted role-controllers.ts
    - deleted role-routes.ts
    - deleted role-services.ts
    - modified index-route.ts to reflect route changes

--- src/Routes/index-route.ts
+++ src/Routes/index-route.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import authRoutes from "./auth-route.js";
-import roleRoutes from "./role-route.js";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRoutes);
-rootRouter.use("/role", roleRoutes);
 
 export default rootRouter;
