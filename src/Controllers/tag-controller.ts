1f539d70fdb230ad73515f2d7e1dc10108c274d5
Pangwaree <66160362@go.buu.ac.th>
2025-09-29 10:32:53 +0700

    feat(tag) : create, edit, delete, getAll tag and add DTO
    เพิ่ม dto tag
    เพิ่มคอมเม้น
    เพิ่ม สิทธิ์เข้าถึงแค่super admin

---
+++ src/Controllers/tag-controller.ts
@@ -0,0 +1,120 @@
+/*
+ * คำอธิบาย : Controller สำหรับการจัดการ Tag
+ * ประกอบด้วยการสร้าง (create), แก้ไข (edit), ลบ (delete), และดึงข้อมูล Tag ทั้งหมด(get)
+ * โดยใช้ TagService ในการทำงานหลัก และส่งผลลัพธ์กลับด้วย createResponse / createErrorResponse
+ */
+import { IsInt, IsNumberString } from "class-validator";
+import { Type } from "class-transformer";
+import type { Request, Response } from "express";
+import * as TagService from "../Services/tag/tag-service.js";
+import { createErrorResponse, createResponse } from "~/Libs/createResponse.js";
+
+import {
+    TagDto,
+} from "~/Services/tag/tag-dto.js";
+
+import {
+    commonDto,
+    type TypedHandlerFromDto,
+} from "~/Libs/Types/TypedHandler.js";
+
+/*
+ * คำอธิบาย : DTO สำหรับสร้างข้อมูลTagใหม่
+ * Input : body (TagDto)
+ * Output : ข้อมูลTagที่ถูกสร้าง
+ */
+export const createTagDto = {
+    body: TagDto,
+} satisfies commonDto;
+
+/*
+ * ฟังก์ชัน : createTag
+ * คำอธิบาย : Handler สำหรับสร้าง Tag ใหม่
+ * Input : req.body - ข้อมูล Tag จาก client
+ * Output :
+ *   - 201 Created พร้อมข้อมูล Tag ที่สร้างใหม่
+ *   - 400 Bad Request ถ้ามี error
+ */
+export const createTag: TypedHandlerFromDto<typeof createTagDto> = async (req, res) => {
+    try {
+        const result = await TagService.createTag(req.body);
+        return createResponse(res, 201, "Tag created successfully", result);
+    } catch (error) {
+        return createErrorResponse(res, 400, (error as Error).message);
+    }
+};
+
+/*
+ * คำอธิบาย : DTO สำหรับตรวจสอบค่า tagId ที่รับมาจาก params
+ * Input : tagId (number)
+ * Output : tagId ที่ถูกตรวจสอบแล้ว
+ */
+export class IdParamDto {
+    @IsNumberString()
+    tagId?: number;
+}
+
+/*
+ * คำอธิบาย : DTO สำหรับลบtagตาม tagId
+ * Input : params (IdParamDto)
+ * Output : ข้อมูลtagที่ถูกลบ
+ */
+export const deleteTagByIdDto = {
+    params: IdParamDto,
+} satisfies commonDto;
+
+/*
+ * คำอธิบาย : ฟังก์ชันสำหรับลบข้อมูลTagตาม tagId
+ * Input : req.params.tagId
+ * Output : JSON response พร้อมข้อมูลTagที่ถูกลบ
+ */
+export const deleteTagById: TypedHandlerFromDto<typeof deleteTagByIdDto> = async (req, res) => {
+    try {
+        const result = await TagService.deleteTagById(Number(req.params.tagId));
+        return createResponse(res, 201, "Tag deleted successfully", result);
+    } catch (error) {
+        return createErrorResponse(res, 400, (error as Error).message);
+    }
+};
+
+/*
+ * คำอธิบาย : DTO สำหรับแก้ไขข้อมูลTag
+ * Input : body (updateTagDto), params (IdParamDto)
+ * Output : ข้อมูลTagที่ถูกแก้ไข
+ */
+export const editTagDto = {
+    body: TagDto,
+    params: IdParamDto,
+} satisfies commonDto;
+
+/*
+ * คำอธิบาย : ฟังก์ชันสำหรับแก้ไขข้อมูลTagที่มีอยู่
+ * Input : req.params.tagId, req.body (name)
+ * Output : JSON response พร้อมข้อมูลTagที่ถูกแก้ไข
+ */
+export const editTag: TypedHandlerFromDto<typeof editTagDto> = async (req, res) => {
+    try {
+        const tagName = req.body;
+        const id = Number(req.params.tagId);
+        const result = await TagService.editTag(id, tagName);
+        return createResponse(res, 201, "Tag edited successfully", result);
+    } catch (error) {
+        return createErrorResponse(res, 400, (error as Error).message);
+    }
+};
+
+/*
+ * ฟังก์ชัน : getAllTags
+ * Input : req.body - ข้อมูลผู้ใช้จาก client
+ * Output :
+ *   - 201 OK พร้อมข้อมูล Tag ทั้งหมด
+ *   - 400 Bad Request ถ้ามี error
+ */
+export const getAllTags = async (req: Request, res: Response) => {
+    try {
+        const result = await TagService.getAllTags();
+        return createResponse(res, 200, "Tags retrieved successfully", result);
+    } catch (error) {
+        return createErrorResponse(res, 400, (error as Error).message);
+    }
+};
