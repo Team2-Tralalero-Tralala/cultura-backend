c87dfc34cb16a25f6b18839529b76545796038f7
Sorayoot Soison <leo.sorayut9@gmail.com>
2025-09-30 22:37:42 +0700

    feat(user): implement user management endpoints and services with pagination support

--- src/Routes/index-route.ts
+++ src/Routes/index-route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth-route.js";
 import roleRoutes from "./role-route.js";
+import userRoutes from "./user-route.js";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRoutes);
 rootRouter.use("/role", roleRoutes);
+rootRouter.use("/users", userRoutes);
 
 export default rootRouter;
