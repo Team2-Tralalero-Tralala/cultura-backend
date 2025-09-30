92312da622c69b8b766944cf4428e309103414e7
Natthanan04 <natthanansirisurayut@gmail.com>
2025-09-30 18:42:43 +0700

    feat(community): add function create edit delete community

---
+++ src/Controllers/community-controller.ts
@@ -0,0 +1,96 @@
+import { IsNumberString } from "class-validator";
+
+import * as CommunityService from "~/Services/community/community-service.js";
+import { CommunityDto } from "~/Services/community/community-dto.js";
+
+import {
+  commonDto,
+  type TypedHandlerFromDto,
+} from "~/Libs/Types/TypedHandler.js";
+import { createErrorResponse, createResponse } from "~/Libs/createResponse.js";
+
+/*
+ * คำอธิบาย : DTO สำหรับสร้างข้อมูลชุมชนใหม่
+ * Input : body (CommunityFormDto)
+ * Output : ข้อมูลชุมชนที่ถูกสร้าง
+ */
+export const createCommunityDto = {
+  body: CommunityDto,
+} satisfies commonDto;
+
+/*
+ * คำอธิบาย : ฟังก์ชันสำหรับสร้างข้อมูลชุมชนใหม่
+ * Input : req.body (community, bankAccount, communityImage,location, homestay, store, member)
+ * Output : JSON response พร้อมข้อมูลชุมชนที่ถูกสร้าง
+ */
+export const createCommunity: TypedHandlerFromDto<
+  typeof createCommunityDto
+> = async (req, res) => {
+  try {
+    const result = await CommunityService.createCommunity(req.body);
+    return createResponse(res, 201, "Community created successfully", result);
+  } catch (error) {
+    return createErrorResponse(res, 400, (error as Error).message);
+  }
+};
+
+/*
+ * คำอธิบาย : DTO สำหรับตรวจสอบค่า communityId ที่รับมาจาก params
+ * Input : communityId (number)
+ * Output : communityId ที่ถูกตรวจสอบแล้ว
+ */
+export class IdParamDto {
+  @IsNumberString()
+  communityId?: string;
+}
+/*
+ * คำอธิบาย : DTO สำหรับแก้ไขข้อมูลชุมชน
+ * Input : body (editCommunityDto), params (IdParamDto)
+ * Output : ข้อมูลชุมชนที่ถูกแก้ไข
+ */
+export const editCommunityDto = {
+  body: CommunityDto,
+  params: IdParamDto,
+} satisfies commonDto;
+/*
+ * คำอธิบาย : ฟังก์ชันสำหรับแก้ไขข้อมูลชุมชนที่มีอยู่
+ * Input : req.params.communityId, req.body (community, location)
+ * Output : JSON response พร้อมข้อมูลชุมชนที่ถูกแก้ไข
+ */
+export const editCommunity: TypedHandlerFromDto<
+  typeof editCommunityDto
+> = async (req, res) => {
+  try {
+    const communityId = Number(req.params.communityId);
+    const result = await CommunityService.editCommunity(communityId, req.body);
+    return createResponse(res, 201, "Update community successfully", result);
+  } catch (error) {
+    return createErrorResponse(res, 400, (error as Error).message);
+  }
+};
+
+/*
+ * คำอธิบาย : DTO สำหรับลบข้อมูลชุมชนตาม communityId
+ * Input : params (IdParamDto)
+ * Output : ข้อมูลชุมชนที่ถูกลบ
+ */
+export const deleteCommunityByIdDto = {
+  params: IdParamDto,
+} satisfies commonDto;
+
+/*
+ * คำอธิบาย : ฟังก์ชันสำหรับลบข้อมูลชุมชนตาม communityId
+ * Input : req.params.communityId
+ * Output : JSON response พร้อมข้อมูลชุมชนที่ถูกลบ
+ */
+export const deleteCommunityById: TypedHandlerFromDto<
+  typeof deleteCommunityByIdDto
+> = async (req, res) => {
+  try {
+    const communityId = Number(req.params.communityId);
+    const result = await CommunityService.deleteCommunityById(communityId);
+    return createResponse(res, 201, "Deleted community successfully", result);
+  } catch (error) {
+    return createErrorResponse(res, 400, (error as Error).message);
+  }
+};
--- src/Routes/index-route.ts
+++ src/Routes/index-route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth-route.js";
+import communityRoutes from "./community-route.js";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRoutes);
+rootRouter.use(communityRoutes);
 
 export default rootRouter;
