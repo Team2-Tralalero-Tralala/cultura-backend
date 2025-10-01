7318e29a8d3e9748147c71816bb9e9fe02df9b5d
TorlapSayHi <torlap.ritchai@gmail.com>
2025-10-01 23:55:02 +0700

    refactor(package): remove package-related controllers, routes, services, and DTOs

--- src/Routes/index-route.ts
+++ src/Routes/index-route.ts
@@ -1,14 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth-route.js";
 import roleRoutes from "./role-route.js";
-import packageRoutes from "./package-route.js";
 import bookingHistoryRoutes from "./booking-history-route.js";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRoutes);
 rootRouter.use("/role", roleRoutes);
-rootRouter.use(packageRoutes);
 rootRouter.use("/booking-histories", bookingHistoryRoutes);
 
 export default rootRouter;
