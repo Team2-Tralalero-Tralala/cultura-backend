128bb4db923bb9b3129881ca77922ddeab6c800b
TeerapolGOD <teerapol94@gmail.com>
2025-10-15 21:05:11 +0700

    refactor(routes): update booking history routes for consistency and clarity

--- src/Routes/booking-history-route.ts
+++ src/Routes/booking-history-route.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { authMiddleware } from "~/Middlewares/auth-middleware.js";
 import { getByRole } from "../Controllers/booking-history-controller.js";
-import * as bookingController from "../Controllers/booking-history-controller.js";
-import { validateDto } from "~/Libs/validateDto.js";
 
 const router = Router();
 
-router.get("/role", authMiddleware, getByRole);
+router.get("/histories", authMiddleware, getByRole);
 
 export default router;
 
