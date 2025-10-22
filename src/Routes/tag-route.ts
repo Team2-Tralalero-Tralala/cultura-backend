c318c6263668c48a073a8ab8346016ee9bb9e220
Natthanan04 <natthanansirisurayut@gmail.com>
2025-10-22 12:09:15 +0700

    fix(tag): fix prefix and delete tag to soft delete

--- src/Routes/tag-route.ts
+++ src/Routes/tag-route.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import {
-    createTag,
-    createTagDto,
-    deleteTagById,
-    deleteTagByIdDto,
-    editTag,
-    editTagDto,
-    getAllTags
+  createTag,
+  createTagDto,
+  deleteTagById,
+  deleteTagByIdDto,
+  editTag,
+  editTagDto,
+  getAllTags,
 } from "../Controllers/tag-controller.js";
 import { validateDto } from "~/Libs/validateDto.js";
 import { allowRoles, authMiddleware } from "~/Middlewares/auth-middleware.js";
@@ -14,29 +14,26 @@ import { allowRoles, authMiddleware } from "~/Middlewares/auth-middleware.js";
 const tagRoutes = Router();
 
 tagRoutes.post(
-    "/",
-    validateDto(createTagDto),
-    authMiddleware,
-    allowRoles("superadmin"),
-    createTag
+  "/super/tag",
+  validateDto(createTagDto),
+  authMiddleware,
+  allowRoles("superadmin"),
+  createTag
 );
 tagRoutes.delete(
-    "/:tagId",
-    validateDto(deleteTagByIdDto),
-    authMiddleware,
-    allowRoles("superadmin"),
-    deleteTagById
+  "/super/tag/:tagId",
+  validateDto(deleteTagByIdDto),
+  authMiddleware,
+  allowRoles("superadmin"),
+  deleteTagById
 );
 tagRoutes.put(
-    "/:tagId",
-    validateDto(editTagDto),
-    authMiddleware,
-    allowRoles("superadmin"),
-    editTag
+  "/super/tags/:tagId",
+  validateDto(editTagDto),
+  authMiddleware,
+  allowRoles("superadmin"),
+  editTag
 );
-tagRoutes.get(
-    "/",
-    getAllTags
-);
+tagRoutes.get("/shared/tags", getAllTags);
 
 export default tagRoutes;
