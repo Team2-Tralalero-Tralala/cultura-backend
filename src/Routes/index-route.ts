a6cd1ba96d2a5878d00459624c513ba28504d5e1
Samitanan <66160376@go.buu.ac.th>
2025-10-26 20:06:29 +0700

    git commit -m "fix(super): resolve merge conflict detail booking"

--- src/Routes/index-route.ts
+++ src/Routes/index-route.ts
@@ -18,6 +18,7 @@ const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRoutes);
 rootRouter.use("/booking", bookingHistoriesRoutes);
+rootRouter.use("/booking-histories", bookingHistoriesRoutes);
 rootRouter.use("/packages", packageRoutes);
 rootRouter.use(userRoutes);
 rootRouter.use(communityRoutes);
