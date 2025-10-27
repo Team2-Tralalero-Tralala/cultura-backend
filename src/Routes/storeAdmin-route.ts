1dcfa152367b07bc1ab971742fc586822255d223
kunkanlaya-Waddang <kxnxpxl@gmail.com>
2025-10-28 03:04:14 +0700

    feat: controller , route and service get store by id role: admin

---
+++ src/Routes/storeAdmin-route.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { getStoreById, storeDto } from "~/Controllers/storeAdmin-contraller.js";
+import { validateDto } from "~/Libs/validateDto.js";
+import { allowRoles, authMiddleware } from "~/Middlewares/auth-middleware.js";
+const storeRoutes = Router();
+// กำหนด endpoint สำหรับดึงข้อมูลร้านค้าตาม ID
+storeRoutes.get("/:storeId", 
+    authMiddleware, 
+    validateDto(storeDto), 
+    allowRoles("admin"),
+    getStoreById
+);
+
+export default storeRoutes;
