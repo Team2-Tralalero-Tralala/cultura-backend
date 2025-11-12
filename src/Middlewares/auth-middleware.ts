d729d078bda3df90fda89f50a1109c34886f1a6a
tawan475 <43491902+tawan475@users.noreply.github.com>
2025-11-12 21:03:45 +0700

    fix(auth-middleware): improve token retrieval and add error logging

--- src/Middlewares/auth-middleware.ts
+++ src/Middlewares/auth-middleware.ts
@@ -21,7 +21,8 @@ export function authMiddleware(
   res: Response,
   next: NextFunction
 ) {
-  const token = req.headers.authorization?.split(" ")[1] || req.cookies.accessToken;
+  const token = req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
+  console.log(token, req.headers, req.cookies);
 
   if (!token) return createErrorResponse(res, 401, "Missing token");
 
@@ -29,6 +30,7 @@ export function authMiddleware(
     req.user = verifyToken(token);
     next();
   } catch (error) {
+    console.log(error);
     return createErrorResponse(res, 401, "Invalid Token");
   }
 }
