ff8654b2a0e55d82db4e2026a026506c4ab898f2
Samitanan <66160376@go.buu.ac.th>
2025-12-01 19:23:10 +0700

    fix(admin): fix booking detail

--- src/Routes/index-route.ts
+++ src/Routes/index-route.ts
@@ -25,7 +25,7 @@ const rootRouter: Router = Router();
 rootRouter.use("/auth", authRoutes);
 rootRouter.use("/booking", bookingHistoriesRoutes);
 rootRouter.use(packageRoutes);
-rootRouter.use("/booking-histories", bookingHistoriesRoutes);
+rootRouter.use("/admin/booking", bookingHistoriesRoutes);
 rootRouter.use("/packages", packageRoutes);
 rootRouter.use(userRoutes);
 rootRouter.use(communityRoutes);
