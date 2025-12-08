dd5012c2c749d1a48d9b9133cb76845c39de8536
Natthanan04 <natthanansirisurayut@gmail.com>
2025-12-08 14:39:21 +0700

    fix(conflict): fix conflict

--- src/Controllers/booking-history-controller.ts
+++ src/Controllers/booking-history-controller.ts
@@ -1,6 +1,5 @@
 import { IsNumberString } from "class-validator";
 import type { Request, Response } from "express";
-import { IsNumberString } from "class-validator";
 import { createResponse, createErrorResponse } from "~/Libs/createResponse.js";
 import { getHistoriesByRole } from "../Services/booking-history-service.js";
 import * as bookingService from "../Services/booking-history-service.js";
