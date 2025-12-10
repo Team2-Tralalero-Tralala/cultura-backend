c7d66f73e97efe7266b45c2cfdd660a9f9717d85
Sarocha Dokyeesun <sarocha28811@gmail.com>
2025-12-10 15:18:24 +0700

    fix(change-pass) : แก้ route

--- src/Routes/user-route.ts
+++ src/Routes/user-route.ts
@@ -761,7 +761,7 @@ userRoutes.patch(
  */
 
 userRoutes.post(
-  "/users/account/change-password/me",
+  "/shared/account/change-password/me",
   authMiddleware,
   allowRoles("superadmin", "admin", "member", "tourist"),
   validateDto(UserController.changePasswordDto),
