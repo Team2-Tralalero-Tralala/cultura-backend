b676a23ce732b8d9db448c35a96c347616b94638
TeerapolGOD <teerapol194@gmail.com>
2025-12-22 01:08:07 +0700

    fix: แก้ไข comment และเพิ่ม dto ตาม standard

--- src/Routes/feedback-routes.ts
+++ src/Routes/feedback-routes.ts
@@ -92,12 +92,10 @@ const feedbackRoutes = Router();
 
 /*
  * คำอธิบาย : Routes สำหรับดึงฟีดแบ็กทั้งหมดของแพ็กเกจ (เฉพาะแอดมิน)
- * Path : /api/admin/package/feedback/:packageId
- * Access : admin
  */
-
 feedbackRoutes.get(
   "/admin/package/feedbacks/:packageId",
+  validateDto(FeedbackController.getPackageFeedbacksForAdminDto),
   authMiddleware,
   allowRoles("admin"),
   FeedbackController.getPackageFeedbacksForAdmin
@@ -285,8 +283,6 @@ feedbackRoutes.get(
 
 /*
  * คำอธิบาย : Routes สำหรับตอบกลับรีวิว (เฉพาะ member)
- * Path : /api/member/feedback/:feedbackId/reply"
- * Access : member
  */
 feedbackRoutes.post(
   "/member/feedback/:feedbackId/reply",
@@ -357,9 +353,9 @@ feedbackRoutes.post(
  *             schema:
  *               $ref: '#/components/schemas/ErrorResponse'
  */
-
 feedbackRoutes.get(
   "/member/package/feedbacks/:packageId",
+  validateDto(FeedbackController.getPackageFeedbacksForMemberDto),
   authMiddleware,
   allowRoles("member"),
   FeedbackController.getPackageFeedbacksForMember
@@ -431,8 +427,6 @@ feedbackRoutes.get(
 
 /*
  * คำอธิบาย : Routes สำหรับตอบกลับรีวิว (เฉพาะ admin)
- * Path : /api/admin/feedback/:feedbackId/reply"
- * Access : admin
  */
 feedbackRoutes.post(
   "/admin/feedback/:feedbackId/reply",
